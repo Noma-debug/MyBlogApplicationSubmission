9da93e9ce0bfadef5d571edb95204cc486b17468
Noma-debug <emilymotsepe@rocketmail.com>
2024-02-04 12:20:24 +0200

    Merger changes

--- db/migrate/20240204100232_add_userid_to_comments.rb
+++ db/migrate/20240204100232_add_userid_to_comments.rb
@@ -1,6 +1,7 @@
 class AddUseridToComments < ActiveRecord::Migration[7.1]
   def change
-    add_column :comments, :\, :string
+    #add_column :comments, :\, :string
+    add_column :comments, :, :string
     add_reference :comments, :user, null: false, foreign_key: true
   end
 end
