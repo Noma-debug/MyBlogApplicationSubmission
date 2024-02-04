c848eb5fdeeb8aa16b80a2218967d3fdfd209f5a
Noma-debug <emilymotsepe@rocketmail.com>
2024-02-04 17:25:10 +0200

    Adding Associations in Models

--- db/migrate/20240204100126_add_userid_to_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddUseridToPosts < ActiveRecord::Migration[7.1]
-  def change
-    #add_column :posts:string
-    add_column :pots, :, :string
-    add_reference :posts, :user, null: false, foreign_key: true
-  end
-end
---
+++ db/migrate/20240204102322_add_userid_to_posts.rb
@@ -0,0 +1,4 @@
+class AddUseridToPosts < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
